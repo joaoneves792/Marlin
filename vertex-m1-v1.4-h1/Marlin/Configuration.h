6c21f01830a8769c188aeb3cc2e89b32fe65ec2a
Joao Miguel Neves <joaoneves792@gmail.com>
2021-03-05 18:47:00 +0000

    replaced bed thermistor

--- vertex-m1-v1.4-h1/Marlin/Configuration.h
+++ vertex-m1-v1.4-h1/Marlin/Configuration.h
@@ -135,8 +135,8 @@
 #define TEMP_SENSOR_0 5
 #define TEMP_SENSOR_1 0
 #define TEMP_SENSOR_2 0
-#define TEMP_SENSOR_BED 5
-#define DIY_THERMISTOR 1
+#define TEMP_SENSOR_BED 1
+//#define DIY_THERMISTOR 1
 
 // This makes temp sensor 1 a redundant sensor for sensor 0. If the temperatures difference between these sensors is to high the print will be aborted.
 //#define TEMP_SENSOR_1_AS_REDUNDANT
